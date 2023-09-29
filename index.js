41d044a18eedb5fbaaf604ccecb907f11cb342fc
Haraksh-CU <haraksh0541.be21@chitkara.edu.in>
2023-09-29 17:21:52 +0530

    fixed changes

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function mobile() {
     hamburger_container.style.display = "none";
   } else {
     var hamburger_container = document.getElementById("menu-container");
-    hamburger_container.innerHTML = ` <div class="menu-item">Home</div>
-                    <div class="menu-item">About</div>
-                    <div class="menu-item">Shop</div>
-                    <div class="menu-item">Contact Us</div>`;
+    hamburger_container.innerHTML = ` <div class="menu-item"><a href="#home">Home</a></div>
+                    <div class="menu-item"><a href="#about">About</a></div>
+                    <div class="menu-item"><a href="#shop">Shop</a></div>
+                    <div class="menu-item"><a href="#contact">Contact Us</a></div>`;
 
     icon.innerHTML = `<i class="fa-solid fa-magnifying-glass fa-3x" style="color: #237262;"></i>
                 <i class="fa-solid fa-cart-shopping fa-3x" style="color: #237262;"></i>`;
@@ -35,10 +35,10 @@ function toggle() {
   var icon = document.getElementById("icon-container");
   var container = document.getElementById("mobile-view");
   if (count % 2 != 0) {
-    container.innerHTML = `<div class="menu-item">Home</div>
-                    <div class="menu-item">About</div>
-                    <div class="menu-item">Shop</div>
-                    <div class="menu-item">Contact Us</div>`;
+    container.innerHTML = `<div class="menu-item"><a href="#home">Home</a></div>
+                    <div class="menu-item"><a href="#about">About</a></div>
+                    <div class="menu-item"><a href="#shop">Shop</a></div>
+                    <div class="menu-item"><a href="#contact">Contact Us</a></div>`;
     container.style.opacity = 1;
     icon.innerHTML = `<i class="fa-solid fa-xmark fa-5x" style="color: #237262;" onclick="toggle()"></i>
     <i class="fa-solid fa-magnifying-glass fa-5x" style="color: #237262;"></i>
